e555a4e0c9e0c87b0d012d7e43324b367dfd6a42
edgarbolton <35745644+edgarbolton@users.noreply.github.com>
2019-09-08 20:31:33 +0100

    Add files via upload

---
+++ SQLNHSOne028b.sql
@@ -0,0 +1,23 @@
+---------------------------------------------------------------------
+-- SQLNHSOne028b.sql
+---------------------------------------------------------------------
+-- SQL code from  'SQL NHS Part One'  © 2019 edgarbolton@outlook.com
+---------------------------------------------------------------------
+-- Author: E.Bolton
+-- Last updated: 31/08/2019 
+---------------------------------------------------------------------
+-- Using LIKE in a WHERE clause with two % wildcards
+---------------------------------------------------------------------
+SELECT TOP(100)
+[Column 2] AS [Surname], 
+[Column 3] AS [Initial],
+[Column 4] AS [Sex],
+[Column 0] AS [GMC Number],
+[Column 5] AS [Main Specialty],
+[Column 7] AS [Organisation Code]
+FROM [NHSSQL].[dbo].[econcur]
+WHERE [Column 2] LIKE '%bay%'
+ORDER BY [Surname]
+
+
+
