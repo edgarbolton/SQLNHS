6e22b01a9f917e66093ee7406655e1f0265e30e9
edgarbolton <35745644+edgarbolton@users.noreply.github.com>
2019-09-10 02:52:48 +0100

    Add files via upload

---
+++ SQLNHSOne033b.sql
@@ -0,0 +1,28 @@
+---------------------------------------------------------------------
+-- SQLNHSOne033b.sql
+---------------------------------------------------------------------
+-- SQL code from  'SQL NHS Part One'  © 2019 edgarbolton@outlook.com
+---------------------------------------------------------------------
+-- Author: E.Bolton
+-- Last updated: 31/08/2019 
+---------------------------------------------------------------------
+-- Use of CASE statement
+---------------------------------------------------------------------
+
+SELECT TOP(100)
+[Column 2] AS [Surname], 
+[Column 3] AS [Initial],
+[Column 4] AS [Sex],
+
+CASE
+    WHEN [Column 4] = 'M' THEN 'Male'
+    WHEN [Column 4] = 'F' THEN 'Female'
+    ELSE 'Uncertain'
+END AS [Gender],
+
+[Column 0] AS [GMC Number],
+[Column 5] AS [Main Specialty],
+[Column 7] AS [Organisation Code]
+FROM [NHSSQL].[dbo].[econcur]
+ORDER BY [Surname]
+
