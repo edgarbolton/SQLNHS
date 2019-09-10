46ac66d8bf122726a20f8b1e70b298d4bcc2b8ce
edgarbolton <35745644+edgarbolton@users.noreply.github.com>
2019-09-10 21:45:05 +0100

    Add files via upload

---
+++ SQLNHSOne038a.sql
@@ -0,0 +1,31 @@
+---------------------------------------------------------------------
+-- SQLNHSOne038a.sql
+---------------------------------------------------------------------
+-- SQL code from  'SQL NHS Part One'  © 2019 edgarbolton@outlook.com
+---------------------------------------------------------------------
+-- Author: E.Bolton
+-- Last updated: 31/08/2019 
+---------------------------------------------------------------------
+-- JOINing two tables
+---------------------------------------------------------------------
+SELECT TOP(100)
+[Column 2] AS [Surname], 
+[Column 3] AS [Initial],
+[Column 4] AS [Sex],
+
+CASE
+    WHEN [Column 4] = 'M' THEN 'Male'
+    WHEN [Column 4] = 'F' THEN 'Female'
+    ELSE 'Uncertain'
+END AS [Gender],
+
+[Column 0] AS [GMC Number],
+[Column 5] AS [Main Specialty],
+[Column 7] AS [Organisation Code],
+[etr].[column2] AS [Trust]
+
+FROM [NHSSQL].[dbo].[econcur]
+JOIN [NHSSQL].[dbo].[etr]
+ON [NHSSQL].[dbo].[econcur].[Column 7]=[NHSSQL].[dbo].[etr].[column1]
+
+ORDER BY [Surname]
