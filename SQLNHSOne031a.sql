ebb0cd4160273ec9e731ec2daf80d314e6a0a887
edgarbolton <35745644+edgarbolton@users.noreply.github.com>
2019-09-10 02:28:07 +0100

    Add files via upload

---
+++ SQLNHSOne031a.sql
@@ -0,0 +1,9 @@
+
+-------------------------------------------------
+-- NHSSQL: ConsultantList.sql
+-------------------------------------------------
+-- List of Consultants for a specified NHS Trust
+-- Uses the table econcur
+-- Based on the NHS England Data Catalogue 
+-- Created E.Bolton. 14/03/2019
+-------------------------------------------------- 
