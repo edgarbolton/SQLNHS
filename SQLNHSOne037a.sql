a6f32ac590aa54e6b40bea319a26a2aaaed55c6c
edgarbolton <35745644+edgarbolton@users.noreply.github.com>
2019-09-10 07:14:15 +0100

    Add files via upload

---
+++ SQLNHSOne037a.sql
@@ -0,0 +1,40 @@
+---------------------------------------------------------------------
+-- SQLNHSOne037a.sql
+---------------------------------------------------------------------
+-- SQL code from  'SQL NHS Part One'  © 2019 edgarbolton@outlook.com
+---------------------------------------------------------------------
+-- Author: E.Bolton
+-- Last updated: 31/08/2019 
+---------------------------------------------------------------------
+-- Creates a table to load etr.csv data into
+---------------------------------------------------------------------
+
+CREATE TABLE [dbo].[etr](
+	[column1] [nvarchar](50) NOT NULL,
+	[column2] [nvarchar](255) NULL,
+	[column3] [nvarchar](50) NULL,
+	[column4] [nvarchar](50) NULL,
+	[column5] [nvarchar](50) NULL,
+	[column6] [nvarchar](50) NULL,
+	[column7] [nvarchar](50) NULL,
+	[column8] [nvarchar](50) NULL,
+	[column9] [nvarchar](50) NULL,
+	[column10] [nvarchar](50) NULL,
+	[column11] [nvarchar](50) NULL,
+	[column12] [nvarchar](50) NULL,
+	[column13] [nvarchar](50) NULL,
+	[column14] [nvarchar](50) NULL,
+	[column15] [nvarchar](50) NULL,
+	[column16] [nvarchar](50) NULL,
+	[column17] [nvarchar](50) NULL,
+	[column18] [nvarchar](50) NULL,
+	[column19] [nvarchar](50) NULL,
+	[column20] [nvarchar](50) NULL,
+	[column21] [nvarchar](50) NULL,
+	[column22] [nvarchar](50) NULL,
+	[column23] [nvarchar](50) NULL,
+	[column24] [nvarchar](50) NULL,
+	[column25] [nvarchar](50) NULL,
+	[column26] [nvarchar](50) NULL,
+	[column27] [nvarchar](50) NULL 
+) 
