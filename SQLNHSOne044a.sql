4812d070e696433898804acf92cb60e8e5d852c9
edgarbolton <35745644+edgarbolton@users.noreply.github.com>
2019-09-13 02:23:34 +0100

    Add files via upload

---
+++ SQLNHSOne044a.sql
@@ -0,0 +1,36 @@
+---------------------------------------------------------------------
+-- SQLNHSOne044a.sql
+---------------------------------------------------------------------
+-- SQL code from  'SQL NHS Part One'  © 2019 edgarbolton@outlook.com
+---------------------------------------------------------------------
+-- Author: E.Bolton
+-- Last updated: 31/08/2019 
+---------------------------------------------------------------------
+-- Joining to Speciliaties table as well as Trusts
+---------------------------------------------------------------------
+SELECT TOP(100)
+[econcur].[Column 2] AS [Surname], 
+[econcur].[Column 3] AS [Initial],
+[econcur].[Column 4] AS [Sex],
+
+CASE
+    WHEN [econcur].[Column 4] = 'M' THEN 'Male'
+    WHEN [econcur].[Column 4] = 'F' THEN 'Female'
+    ELSE 'Uncertain'
+END AS [Gender],
+
+[econcur].[Column 0] AS [GMC Number],
+[econcur].[Column 5] AS [Main Specialty],
+[tlkpSpecialties].[Description], 
+--[econcur].[Column 7] AS [Organisation Code],
+[etr].[column2] AS [Trust]
+
+FROM [NHSSQL].[dbo].[econcur]
+JOIN [NHSSQL].[dbo].[etr]
+ON [NHSSQL].[dbo].[econcur].[Column 7]=[NHSSQL].[dbo].[etr].[column1]
+JOIN [dbo].[tlkpSpecialties]
+ON [econcur].[Column 5]=[tlkpSpecialties].[Code] 
+
+ORDER BY [Surname]
+
+
