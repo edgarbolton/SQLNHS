aabb7c47acaa64474cb069cab4b0c78fdc3ecafa
edgarbolton <35745644+edgarbolton@users.noreply.github.com>
2019-09-19 20:49:27 +0100

    Add files via upload

---
+++ SQLNHSOne063c.sql
@@ -0,0 +1,23 @@
+---------------------------------------------------------------------
+-- SQLNHSOne063c.sql
+---------------------------------------------------------------------
+-- SQL code from  'SQL NHS Part One'  © 2019 edgarbolton@outlook.com
+---------------------------------------------------------------------
+-- Author: E.Bolton
+-- Last updated: 31/08/2019 
+---------------------------------------------------------------------
+-- Calculating percentages
+-- Forcing numbers to float type to avoid integer division
+---------------------------------------------------------------------
+
+SELECT CAST(51 AS FLOAT)/CAST(100 AS FLOAT) AS [51PERCENT] 
+
+
+
+
+
+
+
+
+
+
