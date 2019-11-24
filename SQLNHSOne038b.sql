c10e8e93ca0d492311d0dd0c0214283fc3b590f1
edgarbolton <35745644+edgarbolton@users.noreply.github.com>
2019-11-24 20:28:56 +0000

    Add files via upload

--- SQLNHSOne038b.sql
+++ SQLNHSOne038b.sql
@@ -11,14 +11,12 @@
 SELECT TOP(100)
 [Column 2] AS [Surname], 
 [Column 3] AS [Initial],
-[Column 4] AS [Sex],
-
+--[Column 4] AS [Sex],
 CASE
     WHEN [Column 4] = 'M' THEN 'Male'
     WHEN [Column 4] = 'F' THEN 'Female'
     ELSE 'Uncertain'
 END AS [Gender],
-
 [Column 0] AS [GMC Number],
 [Column 5] AS [Main Specialty],
 --[Column 7] AS [Organisation Code],
